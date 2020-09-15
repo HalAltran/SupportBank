ff3a5030410834d22d5871ea897fc75bce7c17f6
hsmith <hal.smith@altran.com>
2020-09-15 17:24:57 +0100

    Started implemented logging. Not very neat at the moment. Need to add logging for malformed commands (print to console as well).

--- supportBank/Bank.py
+++ supportBank/Bank.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 from decimal import Decimal
 from supportBank import CSVData
@@ -9,6 +10,9 @@ from supportBank.Transaction import Transaction
 class Bank:
     def __init__(self):
         self.accounts = {}
+        self.bank_open = True
+        logging.basicConfig(filename='../log/SupportBank.log', filemode='w', level=logging.DEBUG)
+        self.error_count = 0
 
     def parse_csv_data(self, csv_data: CSVData):
         name_set = set()
@@ -17,9 +21,54 @@ class Bank:
             name_set.add(row.get_entry("To"))
         for name in name_set:
             self.accounts[name] = Account(name)
+        row_count = 2
+        self.error_count = 0
         for row in csv_data.rows:
-            self.create_transaction(row.get_entry("Date"), row.get_entry("From"), row.get_entry("To"),
-                                    row.get_entry("Narrative"), Decimal(row.get_entry("Amount")))
+            self.create_transaction_from_row(row, row_count)
+            row_count += 1
+        logging.info("Bank has parsed CSV Data. " + str(self.error_count) + " rows were rejected.")
+
+    def create_transaction_from_row(self, row, row_count):
+        try:
+            date = row.get_entry("Date")
+            if not re.match(r"[\d]{2}/[\d]{2}/[\d]{4}", date):
+                logging.warning("Row: " + str(row_count) + " contains a date error and could not be parsed. Dates must "
+                                                           "be in the format dd/mm/yyyy.")
+                self.error_count += 1
+                return
+        except:
+            logging.warning("Row: " + str(row_count) + " contains a date error and could not be parsed. Dates must "
+                                                       "be in the format dd/mm/yyyy.")
+            self.error_count += 1
+            return
+        try:
+            account_from = row.get_entry("From")
+        except:
+            logging.warning("Row: " + str(row_count) + " contains an error with the \"From\" column and could not be "
+                                                       "parsed.")
+            self.error_count += 1
+            return
+        try:
+            account_to = row.get_entry("To")
+        except:
+            logging.warning("Row: " + str(row_count) + " contains an error with the \"To\" column and could not be "
+                                                       "parsed.")
+            self.error_count += 1
+            return
+        try:
+            narrative = row.get_entry("Narrative")
+        except:
+            logging.warning("Row: " + str(row_count) + " contains an error with the \"Narrative\" column and could not "
+                                                       "be parsed.")
+            self.error_count += 1
+            return
+        try:
+            amount = Decimal(row.get_entry("Amount"))
+        except:
+            logging.warning("Row: " + str(row_count) + " contains an invalid decimal and could not be parsed.")
+            self.error_count += 1
+            return
+        self.create_transaction(date, account_from, account_to, narrative, amount)
 
     def add_account(self, account: Account):
         self.accounts[account.name] = {account}
@@ -39,10 +88,14 @@ class Bank:
         print(self.accounts[name].list_account())
 
     def do_user_command(self, command: str):
+        logging.info("User performed the following command: \"" + command + "\"")
         if command == "List All":
             self.list_all()
-        if re.match(r"List \[[a-z A-Z]+\]", command):
+        elif re.match(r"List \[[a-z A-Z]+\]", command):
             command = re.sub(r"List \[", "", command)
             name = re.sub(r"\]", "", command)
             if self.accounts.__contains__(name):
                 self.list_account(name)
+        elif command == "end":
+            self.bank_open = False
+            logging.info("Bank has closed for the day.")
--- supportBank/__init__.py
+++ supportBank/__init__.py
@@ -3,6 +3,9 @@ from supportBank.CSVData import CSVData
 
 if __name__ == "__main__":
     bank = Bank()
-    csv_data = CSVData("../res/Transactions2014.csv")
+    # csv_data = CSVData("../res/Transactions2014.csv")
+    csv_data = CSVData("../res/DodgyTransactions2015.csv")
     bank.parse_csv_data(csv_data)
-    bank.do_user_command(input("Type command: "))
+    while bank.bank_open:
+        user_command = input("Type command: ")
+        bank.do_user_command(user_command)
